addb947e8ca922195157f54f00434926c79d39c9
Lütfullah Çelenk <lutfullahclnk1104@gmail.com>
2021-08-23 15:11:15 +0300

    added Header.js file

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+
+const Header = () => {
+    return(
+        <div className="big-title">
+            <h3>POPULAR TOUR PLACES</h3>
+            <p>Click to picture to get more information.</p>
+        </div>
+    )
+}
+
+export default Header;
